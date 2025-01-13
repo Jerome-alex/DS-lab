5f4828bb73751d6a6cf3dd8a0b37910ba6b33e9e
Jerome-alex <137639468+Jerome-alex@users.noreply.github.com>
2025-01-13 11:24:36 +0530

    Update disjoint.c

--- disjoint.c
+++ disjoint.c
@@ -1,141 +1,134 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node{
-   struct node *rep;
-   struct node *next;
-   int data;
-}*heads[50],*tails[50];
+struct node {
+    struct node *rep;
+    struct node *next;
+    int data;
+} *heads[50], *tails[50];
 
-static int countRoot=0;
+static int countRoot = 0;
 
-void makeSet(int x){
-	struct node *new=(struct node *)malloc(sizeof(struct node));
-	new->rep=new;
-	new->next=NULL;
-	new->data=x;
-	heads[countRoot]=new;
-	tails[countRoot++]=new;
+void makeSet(int x) {
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    new->rep = new;   // The representative of a single-element set is itself
+    new->next = NULL; // No next element in the set
+    new->data = x;
+
+    heads[countRoot] = new;  // Add the new set to the list of heads
+    tails[countRoot] = new;  // The tail is the same as the head for a single-element set
+    countRoot++;
 }
 
-struct node* find(int a){
-	int i;
-	struct node *tmp=(struct node *)malloc(sizeof(struct node));
-	for(i=0;i<countRoot;i++){
-		tmp=heads[i];
-		while(tmp!=NULL){
-		if(tmp->data==a)
-		return tmp->rep;
-		tmp=tmp->next;
-		}
- 	}
-	return NULL;
+struct node* find(int a) {
+    for (int i = 0; i < countRoot; i++) {
+        struct node *current = heads[i];
+        while (current != NULL) {
+            if (current->data == a) {
+                return current->rep; // Return the representative of the set
+            }
+            current = current->next;
+        }
+    }
+    return NULL; // Element not found
 }
 
-void unionSets(int a,int b){
-	int i,pos,flag=0,j;
-	struct node *tail2=(struct node *)malloc(sizeof(struct node));
-	struct node *rep1=find(a);
-	struct node *rep2=find(b);
-	if(rep1==NULL||rep2==NULL){
-		printf("\nElement not present in the DS\n");
-		return;
-	}
-	if(rep1!=rep2){
-		for(j=0;j<countRoot;j++){
-			if(heads[j]==rep2){
-				pos=j;
-				flag=1;
-				countRoot-=1;
-				tail2=tails[j];
-				for(i=pos;i<countRoot;i++){
-					heads[i]=heads[i+1];
-					tails[i]=tails[i+1];
-				}
-			}
-			if(flag==1)
-				break;
-		}
-		for(j=0;j<countRoot;j++){
-			if(heads[j]==rep1){
-				tails[j]->next=rep2;
-				tails[j]=tail2;
-				break;
-			}
-		}
-		while(rep2!=NULL){
-		rep2->rep=rep1;
-		rep2=rep2->next;
-		}
-	}
+void unionSets(int a, int b) {
+    struct node *rep1 = find(a);
+    struct node *rep2 = find(b);
+
+    if (rep1 == NULL || rep2 == NULL) {
+        printf("\nOne or both elements not present in the sets.\n");
+        return;
+    }
+
+    if (rep1 != rep2) {
+        for (int i = 0; i < countRoot; i++) {
+            if (heads[i] == rep2) { // Merge the set of `b` into the set of `a`
+                tails[i]->next = NULL; // Detach the merged set from the list
+                for (int j = i; j < countRoot - 1; j++) {
+                    heads[j] = heads[j + 1];
+                    tails[j] = tails[j + 1];
+                }
+                countRoot--;
+                break;
+            }
+        }
+        for (int i = 0; i < countRoot; i++) {
+            if (heads[i] == rep1) {
+                tails[i]->next = rep2; // Attach the second set to the first set
+                struct node *current = rep2;
+                while (current != NULL) {
+                    current->rep = rep1; // Update representative of all nodes in the merged set
+                    tails[i] = current;
+                    current = current->next;
+                }
+                break;
+            }
+        }
+    }
 }
 
-int search(int x){
-	int i;
-	struct node *tmp=(struct node *)malloc(sizeof(struct node));
-	for(i=0;i<countRoot;i++){
-		tmp=heads[i];
-		if(heads[i]->data==x)
-			return 1;	
-		while(tmp!=NULL){
-			if(tmp->data==x)
-				return 1;	
-			tmp=tmp->next;
-		}
-	}
-	return 0;
+int search(int x) {
+    return find(x) != NULL;
 }
 
+void displaySetRepresentatives() {
+    printf("\nSet Representatives: ");
+    for (int i = 0; i < countRoot; i++) {
+        printf("%d ", heads[i]->data);
+    }
+    printf("\n");
+}
 
-void main(){
-int choice,x,i,j,y,flag=0;
-	
-	do{
-		printf("\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-		printf("\n.......MENU.......\n\n1.Make Set\n2.Display set representatives\n3.Union\n4.Find Set\n5.Exit\n");
-	
-		printf("Enter your choice :  ");
-		scanf("%d",&choice);
-		printf("\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-		switch(choice){
-		case 1:	
-			printf("\nEnter new element : ");
-			scanf("%d",&x);
-			if(search(x)==1)
-				printf("\nElement already present in the disjoint set DS\n");
-			else
-				makeSet(x);
-			break;
-		case 2:
-			printf("\n");
-			for(i=0;i<countRoot;i++)
-				printf("%d ",heads[i]->data);
-			printf("\n");
-			break;
-		case 3:
-			printf("\nEnter first element : ");
-			scanf("%d",&x);
-			printf("\nEnter second element : ");
-			scanf("%d",&y);
-			unionSets(x,y);
-			break;
-		case 4:
-			printf("\nEnter the element");
-			scanf("%d",&x);
-			struct node *rep=(struct node *)malloc(sizeof(struct node));
-			rep=find(x);
-			if(rep==NULL)
-			printf("\nElement not present in the DS\n");
-			else
-			printf("\nThe representative of %d is %d\n",x,rep->data);
-			break;
-		case 5:
-			exit(0);
-		default: 
-			printf("\nWrong choice\n");
-			break;
-		}
-	}
-	while(1);
+int main() {
+    int choice, x, y;
 
-};
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Make Set\n");
+        printf("2. Display Set Representatives\n");
+        printf("3. Union\n");
+        printf("4. Find Set\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("\nEnter element to create a set: ");
+                scanf("%d", &x);
+                if (search(x)) {
+                    printf("Element already exists in a set.\n");
+                } else {
+                    makeSet(x);
+                }
+                break;
+            case 2:
+                displaySetRepresentatives();
+                break;
+            case 3:
+                printf("\nEnter the first element: ");
+                scanf("%d", &x);
+                printf("Enter the second element: ");
+                scanf("%d", &y);
+                unionSets(x, y);
+                break;
+            case 4:
+                printf("\nEnter the element to find: ");
+                scanf("%d", &x);
+                struct node *rep = find(x);
+                if (rep) {
+                    printf("The representative of %d is %d.\n", x, rep->data);
+                } else {
+                    printf("Element not found in any set.\n");
+                }
+                break;
+            case 5:
+                printf("Exiting program.\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+}
